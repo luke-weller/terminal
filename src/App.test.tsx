849615264e5d0e0da4688fe04c909f3d118176b9
Luke Weller <33352024+luke-weller@users.noreply.github.com>
2023-02-04 20:59:05 +0000

    Feature/proof of conceptt (#7)
    * amend unit test name
    
    * add basic state management - closes #2
    
    * fix typo
    
    * add basic commands

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Terminal from './App';
 
-test('renders learn react link', () => {
+test('renders terminal text on load', () => {
   render(<Terminal />);
-  const linkElement = screen.getByText(/A terminal line/i);
+  const linkElement = screen.getByText(/Welcome! to the terminal/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
-import './App.css';
-import {Terminal} from "./Terminal/index";
+import React, {useEffect, useMemo} from 'react';
+import {Terminal} from "./Terminal";
+import {useTerminal} from "./Terminal/hooks/useTerminal";
 
 function App() {
+  const {
+    history,
+    pushToHistory,
+    setTerminalRef,
+    resetTerminal,
+  } = useTerminal();
+
+
+  useEffect(() => {
+    resetTerminal();
+
+    pushToHistory(<>
+        <div><strong>Welcome!</strong> to the terminal.</div>
+        <div style={{fontSize: 20}}>It contains <span style={{color: 'yellow'}}><strong>HTML</strong></span>. Awesome, right?</div>
+        <br/>
+        <div>You can write: start or alert , to execute some commands.</div>
+      </>
+    );
+  }, []);
+
+  const commands = useMemo(() => ({
+    'start': async () => {
+      await pushToHistory(<>
+          <div>
+            <strong>Starting</strong> the server... <span style={{color: 'green'}}>Done</span>
+          </div>
+        </>);
+    },
+    'alert': async () => {
+      alert('Hello!');
+      await pushToHistory(<>
+          <div>
+            <strong>Alert</strong>
+            <span style={{color: 'orange', marginLeft: 10}}>
+              <strong>Shown in the browser</strong>
+            </span>
+          </div>
+        </>);
+    },
+  }), [pushToHistory]);
+
   return (
     <div className="App">
-      <Terminal />
+      <Terminal
+        history={history}
+        ref={setTerminalRef}
+        promptLabel={<>Write something awesome:</>}
+        commands={commands}
+      />
     </div>
   );
 }
