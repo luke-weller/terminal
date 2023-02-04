17ba694357c3cd92c9f92fff7f3b94c2c2514bbc
luke-weller <wellerluke@hotmail.com>
2023-02-04 19:26:11 +0000

    amend unit test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Terminal from './App';
 
-test('renders learn react link', () => {
+test('renders terminal text on load', () => {
   render(<Terminal />);
   const linkElement = screen.getByText(/A terminal line/i);
   expect(linkElement).toBeInTheDocument();
