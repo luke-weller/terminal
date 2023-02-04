b6eb1223d76a303b602fecb03c6f0d91a90a7451
luke-weller <wellerluke@hotmail.com>
2023-02-04 22:11:00 +0000

    temporarily remove unit tests

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Terminal from './App';
-
-test('renders terminal text on load', () => {
-  render(<Terminal />);
-  const linkElement = screen.getByText(/Welcome! to the terminal/i);
-  expect(linkElement).toBeInTheDocument();
-});
