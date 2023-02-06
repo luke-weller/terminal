209a389b6cf52ef31586ed8c3c22a3b2423a8ab8
luke-weller <wellerluke@hotmail.com>
2023-02-06 19:47:57 +0000

    enhance terminal functinalitiy for mvp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 
+
+// ensure the app loads
 it('renders without crashing', () => {
   createRoot(document.createElement('root')).render(<h1>portfolio terminal</h1>)
 });
