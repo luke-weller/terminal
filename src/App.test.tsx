a1e27af66b9682b3d407ebfa605316d285c29139
Luke Weller <33352024+luke-weller@users.noreply.github.com>
2023-02-07 08:28:56 +0000

    add styling for pushing typed history to terminal output and amend GitHub workflow (#15)
    * add styling for pushing typed history to terminal output
    
    * add extra skills
    
    * remove unwanted lines
    
    * amend title for unit test and make linkedin open in same browser
    
    * amend gihub workflow for archived files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 // ensure the app loads
 it('renders without crashing', () => {
-  createRoot(document.createElement('root')).render(<h1>portfolio terminal</h1>)
+  createRoot(document.createElement('root')).render(<h1>portfolio-terminal:luke-weller</h1>)
 });
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from "react";
 import { Terminal } from "./Terminal";
 import { useTerminal } from "./Terminal/hooks/useTerminal";
 
-const linkedinUrl = 'https://www.linkedin.com/in/wellerluke/';
+const linkedinUrl = "https://www.linkedin.com/in/wellerluke/";
 
 function App() {
   const { history, pushToHistory, setTerminalRef, resetTerminal } =
@@ -20,40 +20,41 @@ function App() {
     );
   }, [pushToHistory, resetTerminal]);
 
-  // prettier-ignore
   const commands = useMemo(
     () => ({
       clear: async () => {
         await resetTerminal();
       },
+      // prettier-ignore
       help: async () => {
         await pushToHistory(
           <>
             <div>
               <p>
                 <span className="terminal__prompt__label">
-                  lukes-portfolio %{" "}
-                </span>{" "}
-                help
+                  lukes-portfolio %
+                </span>
+                <span className="terminal__prompt__input">help</span>
               </p>
               <p>These are common commands used in various situations:</p>
-              &nbsp;help              |   find out what commands you can runn
+              &nbsp;help            |   get help
               <br />
-              &nbsp;clear             |   clear the terminal
+              &nbsp;clear           |   clear the terminal
               <br />
-              &nbsp;skills            |   see what skills I have
+              &nbsp;skills          |   see what skills I have
               <br />
-              &nbsp;experience        |   explore my experience
+              &nbsp;experience      |   explore my experience
               <br />
-              &nbsp;qualifications    |   checkout my qualifications
+              &nbsp;qualifications  |   see my qualifications
               <br />
-              &nbsp;linkedin          |   open a new tab with my LinkedIn profile
+              &nbsp;linkedin        |   open linkedin
               <br />
-              &nbsp;interests         |   findo out a little more about me personally
+              &nbsp;interests       |   learn about me
               <br />
-              &nbsp;contact           |   get in touch with me
+              &nbsp;contact         |   get in touch with me
               <br />
-              &nbsp;----------------------
+              &nbsp;status          |   Discover employment status
+              <br />
             </div>
           </>
         );
@@ -62,26 +63,25 @@ function App() {
         await pushToHistory(
           <>
             <p>
-              <span className="terminal__prompt__label">
-                lukes-portfolio %{" "}
-              </span>{" "}
-              experience
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">experience</span>
             </p>
             <div>list experience here</div>
           </>
         );
       },
       linkedin: async () => {
-        window.open(linkedinUrl, '_blank', 'noreferrer')
+        window.open(linkedinUrl, "_blank");
         await pushToHistory(
           <>
             <p>
-              <span className="terminal__prompt__label">
-                lukes-portfolio %{" "}
-              </span>{" "}
-              linkedin
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">linkedin</span>
             </p>
-            <div>LinkedIn will have opened in a new tab. Otherwise copy this link into your browser: {linkedinUrl}</div>
+            <div>
+              LinkedIn will have opened in a new tab. Otherwise copy this link
+              into your browser: {linkedinUrl}
+            </div>
           </>
         );
       },
@@ -89,10 +89,8 @@ function App() {
         await pushToHistory(
           <>
             <p>
-              <span className="terminal__prompt__label">
-                lukes-portfolio %{" "}
-              </span>{" "}
-              qualifications
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">qualifications</span>
             </p>
             <div>list qualifications here</div>
           </>
@@ -102,10 +100,8 @@ function App() {
         await pushToHistory(
           <>
             <p>
-              <span className="terminal__prompt__label">
-                lukes-portfolio %{" "}
-              </span>{" "}
-              contact
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">contact</span>
             </p>
             <div>list contact here</div>
           </>
@@ -115,12 +111,21 @@ function App() {
         await pushToHistory(
           <>
             <p>
-              <span className="terminal__prompt__label">
-                lukes-portfolio %{" "}
-              </span>{" "}
-              contact
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">interests</span>
             </p>
             <div>list interests here</div>
+          </>
+        );
+      },
+      status: async () => {
+        await pushToHistory(
+          <>
+            <p>
+              <span className="terminal__prompt__label">lukes-portfolio %</span>
+              <span className="terminal__prompt__input">status</span>
+            </p>
+            <div>Currently employed full time by Netcompany, UK.</div>
           </>
         );
       },
@@ -130,25 +135,71 @@ function App() {
             <div>
               <p>
                 <span className="terminal__prompt__label">
-                  lukes-portfolio %{" "}
-                </span>{" "}
-                skills
+                  lukes-portfolio %
+                </span>
+                <span className="terminal__prompt__input">skills</span>
               </p>
-              These are skills with industry experience:
+              <p>
+                I have aquired a variety of skills from several roles and have
+                detailed them below. If you are intereted in exploring my
+                experience in more detail please run 'experience' in this
+                terminal.
+              </p>
+              <br />
+              <strong>Programming Languages:</strong>
               <br />
               &nbsp;ReactJS
               <br />
+              &nbsp;Javascript
+              <br />
               &nbsp;ASP.NET
               <br />
-              &nbsp;Azure
+              &nbsp;C#
               <br />
-              &nbsp;AWS
+              &nbsp;HTML5
               <br />
-              &nbsp;MySQL
+              &nbsp;CSS
               <br />
-              &nbsp;Typescript
               <br />
-              &nbsp;----------------------
+              <strong>Cloud Services:</strong>
+              <br />
+              &nbsp;Azure Functions
+              <br />
+              &nbsp;Azure SQL
+              <br />
+              &nbsp;Azure CosmosDB
+              <br />
+              &nbsp;AWS Lambda
+              <br />
+              <br />
+              <strong>Development tools:</strong>
+              <br />
+              &nbsp;Git
+              <br />
+              &nbsp;GitHub
+              <br />
+              &nbsp;GitLab
+              <br />
+              &nbsp;Jira
+              <br />
+              <br />
+              <strong>User Experience:</strong>
+              <br />
+              &nbsp;Rapid Prototypes
+              <br />
+              &nbsp;Style Guides
+              <br />
+              &nbsp;User Stories
+              <br />
+              &nbsp;User Interviews
+              <br />
+              &nbsp;Personas
+              <br />
+              &nbsp;Wireframing
+              <br />
+              &nbsp;Figma, Invison, Sketch.app
+              <br />
+              <br />
             </div>
           </>
         );
